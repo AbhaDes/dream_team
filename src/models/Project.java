e5d320cd3783836fe6a8ae1f4b832d2313822e68
AbhaDes <abhadeshpande5@gmail.com>
2025-10-23 21:24:37 +0000

    Adding feilds for classes

---
+++ src/models/Project.java
@@ -0,0 +1,12 @@
+public Project{
+    public String projectName;
+
+    //getter setter methods 
+    public String getProjectName(){
+        return projectName;
+    }
+
+    public void setProjectName(String projectName){
+        this.projectName = projectName;
+    }
+}
---
+++ src/models/User.java
@@ -0,0 +1,30 @@
+public User{
+    public String name;
+    public String hackerId;
+    public String preferredRole;
+    //constructor 
+
+
+    //getter and setter methods 
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getHackerId(){
+        this.hackerId;
+    }
+    public void getHackerId(this.hackerId){
+        this.hackerId = hackerId;
+        
+    }
+
+    public String getPreferredRole(){
+        return preferredRole;
+    }
+    public void setPreferredRole(String preferredRole){
+        this.preferredRole = preferredRole;
+    }
+}
