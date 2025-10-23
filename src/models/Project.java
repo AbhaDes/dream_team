7926ee499f652514bf5c95ba2292ee39b6e6d240
AbhaDes <abhadeshpande5@gmail.com>
2025-10-23 21:30:51 +0000

    Adding Project class

--- src/models/Project.java
+++ src/models/Project.java
@@ -1,6 +1,11 @@
 public Project{
     public String projectName;
 
+    //constructor
+    public Project(String projectName){
+        this.projectName = projectName;
+    }
+
     //getter setter methods 
     public String getProjectName(){
         return projectName;
