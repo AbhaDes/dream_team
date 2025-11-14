b0220c1b16df694f707e8efeeefe22d18e2d2e39
AbhaDes <abhadeshpande5@gmail.com>
2025-11-14 02:17:29 +0000

    Set up the users table

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,7 @@
-CREATE TABLE users{
-    user_id UUID PRIMARY KEY gen_random_uuid(),
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(60) NOT NULL, 
-    name VARCHAR(100) NOT NULL, 
-    joined_at TIMESTAMP NOT NULL, 
-   
-}
-
-CREATE TABLE events{
-    event_id UUID PRIMARY KEY gen_random_uuid(),
-    event_name VARCHAR(300) NOT NULL,
-    start_date TIMESTAMP NOT NULL, 
-    status VARCHAR(100) NOT NULL, 
-    end_date TIMESTAMP NOT NULL, 
-    location VARCHAR(300) NOT NULL,
-    description VARCHAR(1000) NOT NULL
-}
+CREATE TABLE users (
+    user_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
