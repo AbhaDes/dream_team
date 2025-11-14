38f35bfa1c17e7fbd37ed1bb3612d4407d568a88
AbhaDes <abhadeshpande5@gmail.com>
2025-11-14 01:20:01 +0000

    Set up PostgreSQL connection and database config

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ var message = 'CSC-317 node/express app \n'
          + 'This uses nodeJS, express, and express.static\n'
          + 'to \"serve\" the files in the ./public/ dir!\n';
 
-var express = require('express');
-var app = express();
-var port = 3001;
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/api/test', async(req, res)=>{ //req - what comes in from the browser, res - what the browser sends back
+    res.send('Server is running!');
+});
 
-var path = require('path');
-var StaticDirectory = path.join(__dirname, 'public');
+const port = 3001;
+// Add this line
+const pool = require('./config/database');
+console.log('Pool imported:', pool); // Should show Pool object
+
+const path = require('path');
+const StaticDirectory = path.join(__dirname, 'public');
 app.use(express.static(StaticDirectory));
 
 app.listen(port, () => {
