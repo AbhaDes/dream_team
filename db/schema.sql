501bd24f0b9ba377dbd56056fe9a935fecac158c
AbhaDes <abhadeshpande5@gmail.com>
2025-11-14 01:47:39 +0000

    User and Event schema table

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,18 @@
 CREATE TABLE users{
-    user_id UUID PRIMARY KEY;
-    email
-    
+    user_id UUID PRIMARY KEY gen_random_uuid(),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(60) NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
+    joined_at TIMESTAMP NOT NULL, 
+   
+}
+
+CREATE TABLE events{
+    event_id UUID PRIMARY KEY gen_random_uuid(),
+    event_name VARCHAR(300) NOT NULL,
+    start_date TIMESTAMP NOT NULL, 
+    status VARCHAR(100) NOT NULL, 
+    end_date TIMESTAMP NOT NULL, 
+    location VARCHAR(300) NOT NULL,
+    description VARCHAR(1000) NOT NULL
 }
