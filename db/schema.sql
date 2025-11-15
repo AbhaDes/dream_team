9e46181325ee5540dc2eff6c41e41e84f73a7faa
AbhaDes <abhadeshpande5@gmail.com>
2025-11-15 00:41:25 +0000

    Some database changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     user_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL,
-    name VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
