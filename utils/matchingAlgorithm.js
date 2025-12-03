6491577f470015123bbd35e72fa70bfd0cfd449b
AbhaDes <abhadeshpande5@gmail.com>
2025-12-03 01:10:45 +0000

    Roles added and Matching function started

--- utils/matchingAlgorithm.js
+++ utils/matchingAlgorithm.js
@@ -1,10 +1,13 @@
-const calculateCompatibility = async (req, res, next => {
+function getCompatibilityScore (role1, role2){
     //1. Take two users, user 1 and user 2 as input
     //2. Calculate their compatibility score based on Complementary roles (40 points)
-    //-- how will I determine compatibility for complementary roles? 
+    //-- how will I determine compatibility for complementary roles?
+    //-- Let's focus on complementary roles for now. 
+    //-- Backend + frontend -- 40, Backend + designer -- , Backend +    
     //3. Experience level match (25 points)
     //4. Similar availability (20 points)
     //5. Similar interests/skills(15 points)
 
 
-})
+}
+
