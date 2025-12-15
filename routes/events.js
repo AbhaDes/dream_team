f63e74aa44ec26c8df1189ae25c49511acd47a5f
AbhaDes <abhadeshpande5@gmail.com>
2025-12-15 23:02:26 +0000

    Matching functionality established

--- routes/events.js
+++ routes/events.js
@@ -8,11 +8,11 @@ const { route } = require('./auth');
 //1. Joining an event
 router.post('/:eventId/join', authMiddleware, eventController.joinEvent);
 
-//2. Getting Participant Profile 
-router.get('/:eventId/participants/:participantId', authMiddleware, eventController.getParticipant);
-
-//3. Getting my own profile 
+//2. Getting my own profile
 router.get('/:eventId/participants/me', authMiddleware, eventController.getMe);
+
+//3. Getting the participant profile
+router.get('/:eventId/participants/:participantId', authMiddleware, eventController.getParticipant);
 
 //4. Updating my own profile 
 router.put('/:eventId/participants/me', authMiddleware, eventController.updateProfile );
