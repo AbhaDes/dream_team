5efe71eab81f6f12d3b7cc7796d92ab6a4a476b4
Abha Deshpande <abhadeshpande@Abhas-Laptop.local>
2026-06-26 16:42:15 -0700

    Rendered, trying to connect database

--- config/database.js
+++ config/database.js
@@ -2,9 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 // require('dotenv').config();
 // const { Pool } = require('pg');
 // const pool = new Pool({
