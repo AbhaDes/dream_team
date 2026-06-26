268f52f200c2727e65f11d44bbc2e0533a7f5b14
Abha Deshpande <abhadeshpande@Abhas-Laptop.local>
2026-06-26 16:55:05 -0700

    Current event id added for production

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 //this is used to define one event_id used throughout the app for the MVP 
 module.exports = {
-    CURRENT_EVENT_ID : "19848c8a-790b-4059-aea4-b3c97c6a4e02", 
+    CURRENT_EVENT_ID : "984fe278-f734-4222-a614-5a5f19f3d798", 
     CURRENT_EVENT_NAME : "SFSU Fall Hackathon 2025"
 };
