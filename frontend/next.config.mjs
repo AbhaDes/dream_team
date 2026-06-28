88499e99a0834c0c5001816246335d6208007ba5
Abha Deshpande <abhadeshpande@Abhas-Laptop.local>
2026-06-28 15:28:55 -0700

    Changed the url, falling back to localhost in development

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,12 +7,14 @@ const nextConfig = {
     unoptimized: true,
   },
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
-      },
-    ]
+  return [
+    {
+      source: '/api/:path*',
+      destination: process.env.NEXT_PUBLIC_API_URL 
+        ? `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
+        : 'http://localhost:3001/api/:path*',
+    },
+  ]
   }
 }
 
