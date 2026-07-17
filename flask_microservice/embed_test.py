37d9f3a87c680587329c0c39f2fa0ed319e7c886
Abha Deshpande <abhadeshpande5@gmail.com>
2026-07-17 09:36:36 -0700

    emebed_test.py and mathAlgo.py added with some test code

---
+++ flask_microservice/embed_test.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import matchAlgo
+
+load_dotenv()
+client = OpenAI()
+
+
+def get_embeddings(sentence, model="text-embedding-3-small"):
+
+    response = client.embeddings.create(
+        input=[sentence],
+        model=model
+    )
+    return response.data[0].embedding
+
+vector1 = get_embeddings("A backend developer who loves python")
+vector2 = get_embeddings("A frontend developer who loves react")
+vector3 = get_embeddings("A product manager who loves to plan products")
+
+print(matchAlgo.cosine_similarity(vector1, vector2))
+print(matchAlgo.cosine_similarity(vector1, vector3))
+
+
+
+
+
